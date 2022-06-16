634a5015c4d2f14b8f6c9d108c827962afb42761
James Parrish <jthomasparrishjr@gmail.com>
2022-06-15 20:23:29 -0500

    commit tree 4c8005b750451cd2c0f294255c74bab75750eafa

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Add your code here
+ function submitData(name, email){
+    const data = {
+        name: name,
+        email: email,
+    }
+    return fetch("http://localhost:3000/users", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          "Accept": "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (object) {
+        // dom shit to append object.id
+        const div = document.createElement('div');
+        div.innerHTML =`${object.id}`;
+
+        document.body.appendChild(div)
+      })
+      .catch(function (error) {
+        alert("Bad things! Ragnarők!");
+        document.body.innerHTML =`<p>${error.message}</p>`;
+      });
+ }
